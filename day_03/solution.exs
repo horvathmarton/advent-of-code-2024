533022686e41a9150a48025454f86654566c947f
Marton Horvath <17813215+horvathmarton@users.noreply.github.com>
2024-12-06 10:11:15 +0100

    feat: solution for day 3 task 2

--- day_03/solution.exs
+++ day_03/solution.exs
@@ -1,23 +1,41 @@
 defmodule Solution03 do
   def solve_1(input) do
-    matches = get_all_occurance(input, ~r/mul\([0-9]{1,3},[0-9]{1,3}\)/)
+    matches = get_all_occurance(input, ~r/mul\(\d{1,3},\d{1,3}\)/)
 
-    Enum.map(matches, fn match -> get_all_occurance(match, ~r/[0-9]+/) end)
-    |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
-    |> Enum.map(fn nums -> Enum.product(nums) end)
-    |> Enum.sum
+    Enum.map(matches, &parse_mul/1)
+    |> Enum.sum()
   end
 
   def solve_2(input) do
-    input
+    ignore_pattern = ~r/mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)/
+    matches = get_all_occurance(input, ignore_pattern)
+
+    process_matches(matches, %{ignore: false})
+      |> Enum.map(&parse_mul/1)
+      |> Enum.sum()
+  end
+
+  def parse_mul(match) do
+    get_all_occurance(match, ~r/\d+/)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.product()
   end
 
   def get_all_occurance(string, regex) do
     Regex.scan(regex, string)
-      |> List.flatten()
+    |> List.flatten()
   end
 
-  def mul(first, second) do
-    first * second
+  def process_matches([], %{ignore: _}), do: []
+  def process_matches([match | rest], %{ignore: ignore}) do
+    cond do
+      match == "do()" -> process_matches(rest, %{ignore: false})
+
+      match == "don't()" -> process_matches(rest, %{ignore: true})
+
+      !ignore -> [match] ++ process_matches(rest, %{ignore: ignore})
+
+      true -> process_matches(rest, %{ignore: ignore})
+    end
   end
 end
--- day_03/test.exs
+++ day_03/test.exs
@@ -12,8 +12,8 @@
       end
 
       def test_2() do
-        input = ""
-        expected_result = 4
+        input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        expected_result = 48
 
         test(expected_result, Solution03.solve_2(input), "day 03 / test 2")
       end
